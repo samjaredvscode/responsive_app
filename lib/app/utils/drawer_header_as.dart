60deb7b467022edf27edbea08eaeac352e5c184d
unknown <samajred.02@gmail.com>
2022-12-17 18:22:53 -0500

    new drawer header

---
+++ lib/app/utils/drawer_header_as.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_app/app/finals/items_title.dart';
+
+class DrawerHeaderAS extends StatelessWidget {
+  const DrawerHeaderAS({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.onInverseSurface,
+            borderRadius: BorderRadius.circular(22),
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                left: 2,
+                top: 5,
+                child: IconButton(
+                  icon: const Icon(
+                    Icons.close_rounded,
+                    size: 30,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 80,
+                  horizontal: 10,
+                ),
+                child: Text(
+                  presentation.first.title,
+                  style: const TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
