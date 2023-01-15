f69bda752e6179c452223eef64128eb804cfa59a
samjaredvscode <samjared.02@gmail.com>
2023-01-15 18:59:43 -0500

    removing a variable

--- lib/app/finals/items_title.dart
+++ lib/app/finals/items_title.dart
@@ -2,65 +2,38 @@ import 'package:flutter/material.dart';
 
 import '../models/models_for_list.dart';
 
-final List<ModelListTema> temaList = [
+final itemOnList = [
   ModelListTema(
     title: 'Inicio',
-    icon: const Icon(Icons.adobe_rounded),
+    icon: Icons.adobe_rounded,
   ),
   ModelListTema(
     title: 'Eventos',
-    icon: const Icon(Icons.celebration_rounded),
+    icon: Icons.celebration_rounded,
   ),
   ModelListTema(
     title: 'Carreras',
-    icon: const Icon(Icons.dashboard_customize_rounded),
+    icon: Icons.dashboard_customize_rounded,
   ),
   ModelListTema(
     title: 'Extención',
-    icon: const Icon(Icons.extension_rounded),
+    icon: Icons.extension_rounded,
   ),
   ModelListTema(
     title: 'Información',
-    icon: const Icon(Icons.fact_check_rounded),
+    icon: Icons.fact_check_rounded,
   ),
   ModelListTema(
     title: 'Intranet',
-    icon: const Icon(Icons.group_work_rounded),
+    icon: Icons.group_work_rounded,
   ),
   ModelListTema(
     title: 'Nosotros',
-    icon: const Icon(Icons.info_rounded),
+    icon: Icons.info_rounded,
   ),
 ];
 
-final List<ModelsListCover> cardItems = [
-  ModelsListCover(
-    id: 1,
-    title: 'Most Popular',
-    leading: 'This is a popular plant in our store',
-    icon: const Icon(Icons.auto_awesome_rounded),
-  ),
-  ModelsListCover(
-    id: 2,
-    title: 'Easy care',
-    leading: 'This plant is apropiate for beginners',
-    icon: const Icon(Icons.assignment_rounded),
-  ),
-  ModelsListCover(
-    id: 3,
-    title: 'Faux Available',
-    leading: 'Get the same look without the maintenance',
-    icon: const Icon(Icons.park_rounded),
-  ),
-  ModelsListCover(
-    id: 4,
-    title: 'Better',
-    leading: 'Better for you page and home',
-    icon: const Icon(Icons.grade),
-  ),
-];
-
-final List<PresentationAS> presentation = [
+final presentation = [
   PresentationAS(
     title: 'American System',
     subtitle: 'Instituto de Educación Superior Tecnológico Privado I.E.S.T.P',
