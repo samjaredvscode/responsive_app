49f4224e979d599de0ff401a41ac8a9c77c926e0
samjaredvscode <samjared.02@gmail.com>
2023-01-21 01:32:06 -0500

    new file const and modify items title

---
+++ lib/app/finals/constantes.dart
@@ -0,0 +1,3 @@
+const double kMaxWidthDevices = 345;
+const double kMaxHeightDevices = 785;
+const double kSlidePercent = 75;
--- lib/app/finals/items_title.dart
+++ lib/app/finals/items_title.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_app/app/ui/about_view.dart';
+import 'package:responsive_app/app/ui/events_view.dart';
+import 'package:responsive_app/app/ui/extension_view.dart';
+import 'package:responsive_app/app/ui/information_view.dart';
+import 'package:responsive_app/app/ui/intranet_view.dart';
+import 'package:responsive_app/app/ui/kick-off_view.dart';
+import 'package:responsive_app/app/ui/racing_view.dart';
 
 import '../models/models_for_list.dart';
 
 final itemOnList = [
   ModelListTema(
+    id: 0,
     title: 'Inicio',
     icon: Icons.adobe_rounded,
     subtitle: [],
+    screens: const KickOffView(),
   ),
   ModelListTema(
+    id: 1,
     title: 'Eventos',
     icon: Icons.celebration_rounded,
     subtitle: [],
+    screens: const EventsView(),
   ),
   ModelListTema(
+    id: 2,
     title: 'Carreras',
     icon: Icons.dashboard_customize_rounded,
     subtitle: [
@@ -21,16 +33,20 @@ final itemOnList = [
       'Administración de Redes y Comunicaciones',
       'Secretariado Ejecutivo Computarizado',
     ],
+    screens: const RacingView(),
   ),
   ModelListTema(
+    id: 3,
     title: 'Extención',
     icon: Icons.extension_rounded,
     subtitle: [
       'Curso de Inglés',
       'Cursos básicos de computación',
     ],
+    screens: const ExtensionView(),
   ),
   ModelListTema(
+    id: 4,
     title: 'Información',
     icon: Icons.fact_check_rounded,
     subtitle: [
@@ -42,8 +58,10 @@ final itemOnList = [
       'Registros académicos',
       'Manual del usuario',
     ],
+    screens: const InformationView(),
   ),
   ModelListTema(
+    id: 5,
     title: 'Intranet',
     icon: Icons.group_work_rounded,
     subtitle: [
@@ -51,11 +69,14 @@ final itemOnList = [
       'Administrativo',
       'Docentes',
     ],
+    screens: const IntranetView(),
   ),
   ModelListTema(
+    id: 6,
     title: 'Nosotros',
     icon: Icons.info_rounded,
     subtitle: [],
+    screens: const AboutView(),
   ),
 ];
 
